0aa8fc25fc4ac2c41bdfa515178df134f7c57f96
AAKASH <aakashaggarwal2311@gmail.com>
2024-05-14 02:59:15 +0530

    Refactor:override

--- king/src/main/java/com/King/service/WarriorKillCountServiceInterface.java
+++ king/src/main/java/com/King/service/WarriorKillCountServiceInterface.java
@@ -5,4 +5,11 @@ import java.util.Map;
 public interface WarriorKillCountServiceInterface {
 
     void ProcessData(Map<String, String> data);
+
+    void updateWarriorKills(Map<String, String> killerCodeKill);
+
+    void setWarriorCodeName(Map<String, String> warriorCodeName);
+
+    Map<String, Integer> getTop5Warriors();
+
 }
